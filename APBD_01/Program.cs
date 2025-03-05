0dcaa8f24c2510bef6918560f4bf34dbfe5e31da
JakubGralinski <jakubm.gralinski@gmail.com>
2025-03-05 10:27:49 +0100

    Added average function

--- APBD_01/Program.cs
+++ APBD_01/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class AverageCalculator
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+        {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        return (double)sum / numbers.Length;
+    }
+
+    static void Main()
+    {
+        int[] testArray = { 1, 2, 3, 4, 5 };
+        Console.WriteLine("Average: " + CalculateAverage(testArray));
+    }
+}
